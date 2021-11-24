9a826539b502a21b2a75130fe0519ece189d12a6
Alejandra Murillo <alemurillo104@gmail.com>
2021-11-24 12:15:41 -0400

    classname error fixed v1

---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const handleLogout = () => {
+
+        //TODO: Por hacer
+        console.log('logOut');
+    }
+
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+
+                        className={ ({ isActive }) => 'nav-item nav-link ' + ( isActive  ? 'active' : '' ) } 
+                        exact="true"
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        className={ ({ isActive }) => 'nav-item nav-link ' + ( isActive  ? 'active' : '' ) } 
+                        // className={ ( { isActive } ) => {
+
+                        //     return ( isActive ) 
+                        //     ? 'nav-item nav-link active'
+                        //     : 'nav-item nav-link'
+                        // }}  
+                        exact="true"
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+
+                    <span className="nav-item nav-link text-info">
+                        Alejandra
+                    </span>
+
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={ handleLogout }
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
